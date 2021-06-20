5d233b1785d9db62991c0bfb71a494024153f5c8
Fabio <fabioheiji@gmail.com>
2021-06-20 03:41:15 -0300

    Implementação do Quicksort. Ordenação da maior para menor carta

--- Card.java
+++ Card.java
@@ -3,34 +3,44 @@ public class Card {
 	private int rank;
 	private char suit;
 
-	public Card(int rank, char suit){
+	public Card(int rank, char suit) {
 
 		this.rank = rank;
 		this.suit = suit;
 	}
 
-	public Card(String value){
+	public Card(String value) {
 
-		this.rank = Integer.parseInt(value.substring(0, value.length()-1));
-		this.suit = value.charAt(value.length()-1);
+		this.rank = Integer.parseInt(value.substring(0, value.length() - 1));
+		this.suit = value.charAt(value.length() - 1);
 	}
 
-	public int getRank(){
+	public int getRank() {
 
 		return this.rank;
 	}
 
-	public char getSuit(){
+	public char getSuit() {
 
 		return this.suit;
 	}
 
-	public String toString(){
+	// Setter para a variável rank
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	// Setter para a variável suit
+	public void setSuit(char suit) {
+		this.suit = suit;
+	}
+
+	public String toString() {
 
 		return "" + this.rank + this.suit;
 	}
 
-	public int compareCard(Card other){
+	public int compareCard(Card other) {
 
 		return this.getRank() - other.getRank();
 	}
--- Table.java
+++ Table.java
@@ -79,7 +79,7 @@ public class Table {
 	private boolean isStraight(Card[] cards) {
 
 		for (int i = 1; i < cards.length; i++) {
-			if ((cards[i - 1].getRank() + 1) != cards[i].getRank())
+			if ((cards[i - 1].getRank() - 1) != cards[i].getRank())
 				return false;
 		}
 
@@ -116,7 +116,7 @@ public class Table {
 		// Como este método será chamado por último, caso identifique uma carta com
 		// mesmo rank podemos já assumir que é um par
 		for (int i = 1; i < cards.length; i++) {
-			if ((cards[i - 1].getRank() + 1) == cards[i].getRank())
+			if ((cards[i - 1].getRank()) == cards[i].getRank())
 				return true;
 		}
 		return false;
@@ -126,8 +126,47 @@ public class Table {
 	public String individualTest(String cardsString) {
 
 		Player p = new Player(cardsString);
+		String classificacao = Table.hands[this.defineHand(p)];
+		return p + classificacao;
+	}
 
-		return cardsString + " " + Table.hands[this.defineHand(p)];
+	// Algoritmo quicksort para ordenar o arrajo de cartas
+	private void quicksort(Card[] cards, int p, int r) {
+		if (p < r) {
+			int q = particiona(cards, p, r);
+			quicksort(cards, p, q - 1);
+			quicksort(cards, q + 1, r);
+		}
+	}
+
+	// Eu troco os valores entre as cartas. Utilizo esta função dentro do algoritmo
+	// QuickSorter, mais especificamente no método particiona
+	private void trocaValorEntreCards(Card carta1, Card carta2) {
+		int aux1 = carta1.getRank();
+		char aux2 = carta1.getSuit();
+		carta1.setRank(carta2.getRank());
+		carta1.setSuit(carta2.getSuit());
+		carta2.setRank(aux1);
+		carta2.setSuit(aux2);
+	}
+
+	// reorganiza o subarranjo cards[p ..r] para o algoritmo quicksort
+	private int particiona(Card[] cards, int p, int r) {
+		Card x = cards[r];
+		int i = p - 1;
+
+		for (int j = p; j < r; j++) {
+			if (cards[j].compareCard(x) > 0) {
+				i++;
+				trocaValorEntreCards(cards[i], cards[j]);
+			}
+		}
+		trocaValorEntreCards(cards[i + 1], cards[r]);
+		return i + 1;
+	}
+
+	private void sortCards(Card[] cards) {
+		quicksort(cards, 0, cards.length - 1);
 	}
 
 }
