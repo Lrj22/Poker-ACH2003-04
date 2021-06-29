a5d745c6b677f2d03c1bd4f601b0187d5b123071
Lrj22 <86637124+Lrj22@users.noreply.github.com>
2021-06-28 22:32:58 -0300

    Novos casos de teste.
    Teste para Pair e TwoPair, respectivamente, test5 e test6.

--- PokerGameTest.java
+++ PokerGameTest.java
@@ -25,9 +25,12 @@ public class PokerGameTest {
 
         test1 = "14o 13o 12o 11o 10o";
         test2 = "14o 13c 12o 11o 9o";
-
+        test5 = "14o 13o 10c 11o 10o";
+        test6 = "14o 11c 10c 11o 10o";
         test1Result = "14o 13o 12o 11o 10o RoyalFlush";
         test2Result = "14o 13c 12o 11o 9o ";
+        test5Result = "14o 13o 10c 11o 10o Pair";
+        test6Result = "14o 11c 10c 11o 10o TwoPair";
     }
 
     @Rule
@@ -43,5 +46,15 @@ public class PokerGameTest {
     public void test2() {
 
         assertEquals(test2Result, table.individualTest(test2));
+    }
+    @Test
+    public void test5() {
+
+        assertEquals(test5Result, table.individualTest(test5));
+    }
+    @Test
+    public void test6() {
+
+        assertEquals(test6Result, table.individualTest(test6));
     }
 }
